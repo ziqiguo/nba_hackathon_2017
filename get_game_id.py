15312c09982af071ddaeaaa21771801b3532bea2
mehul <mgoyal3@illinois.edu>
2017-09-23 13:23:49 -0700

    added script to return game id

---
+++ get_game_id.py
@@ -0,0 +1,26 @@
+import csv, sys
+
+def read_csv(filename):
+  rows = []
+  with open(filename, 'rb') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)
+    for row in reader:
+      rows.append(row)
+  return rows
+
+def get_game_id(team1, team2, date):
+  potential_games = []
+  for row in read_csv('data/Game_Map.csv'):
+    if row[2] == date:
+      potential_games.append(row[0])
+  boxscores = read_csv('data/Player_Boxscores.csv')
+  boxscores = [score for score in boxscores if score[0] in potential_games]
+  for game in potential_games:
+    for row in boxscores:
+      if row[10] == team1 or row[10] == team2:
+        return game
+
+if len(sys.argv) != 4:
+  print 'usage: python get_game_id.py <team-abbrev-1> <team-abbrev-2> <date>'
+print get_game_id(sys.argv[1], sys.argv[2], sys.argv[3])
