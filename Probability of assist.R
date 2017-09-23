8137e5b7a850b14b0f267884727efbaf428fa81f
SARAH BAILEY <sarahbailey@SARAHs-MacBook-Pro.local>
2017-09-23 15:40:25 -0400

    Easier to replicate

--- Probability of assist.R
+++ Probability of assist.R
@@ -1,24 +1,35 @@
-# Read in Data and clean
-library(readr)
-library(data.table)
+# Load Packages
+install.packages("pacman")
+library(pacman)
+pacman::p_load(dplyr, readr, data.table)
+
+# Read in Data and clean ---------
+# 2014 ---------
 shot2014 <- read_delim("~/Desktop/Documents/Statistics/Hackathon/Competition/Hackathon/Player Tracking Data/2014-15_nba_shot_log.txt", 
                                       "\t", escape_double = FALSE, trim_ws = TRUE)
+
 shot2014 <- as.data.frame(shot2014)
 
 shot2014$GAME_ID <- as.integer(shot2014$GAME_ID)
+
+# Order in increasing order by game id, period, and decreasing by game clock
+
 shot2014 <- shot2014[
   order( shot2014$GAME_ID, shot2014$PERIOD, -shot2014$GAME_CLOCK ),
   ]
 
+# Create counter for shots taken
 shot2014$shot_count <- with(shot2014, ave(as.character(GAME_ID), GAME_ID, FUN= seq_along))
 
-# 2015
+# Repeat for 2015 ----------
 
 shot2015 <- read_delim("~/Desktop/Documents/Statistics/Hackathon/Competition/Hackathon/Player Tracking Data/2015-16_nba_shot_log.txt", 
                        "\t", escape_double = FALSE, trim_ws = TRUE)
+
 shot2015 <- as.data.frame(shot2015)
 
 shot2015$GAME_ID <- as.integer(shot2015$GAME_ID)
+
 shot2015 <- shot2015[
   order( shot2015$GAME_ID, shot2015$PERIOD, -shot2015$GAME_CLOCK ),
   ]
@@ -26,9 +37,11 @@ shot2015 <- shot2015[
 shot2015$shot_count <- with(shot2015, ave(as.character(GAME_ID), GAME_ID, FUN= seq_along))
 
 
-# 2016 
+# Repeat for 2016 ---------
+
 shot2016 <- read_delim("~/Desktop/Documents/Statistics/Hackathon/Competition/Hackathon/Player Tracking Data/2016-17_nba_shot_log.txt", 
                        "\t", escape_double = FALSE, trim_ws = TRUE)
+
 shot2016 <- as.data.frame(shot2016)
 
 shot2016$GAME_ID <- as.integer(shot2016$GAME_ID)
@@ -39,33 +52,44 @@ shot2016 <- shot2016[
 
 shot2016$shot_count <- with(shot2016, ave(as.character(GAME_ID), GAME_ID, FUN= seq_along))
 
+# Read in play by play data ----------
 
 playbyplay <- fread("~/Desktop/Documents/Statistics/Hackathon/Competition/Hackathon/data/Play_by_Play_New.csv") 
 
-# Filter by year
+# Filter by year ----------
+
+#2014
 playbyplay14 <- playbyplay[playbyplay$Season==2014,]
 playbyplay14 <- playbyplay14[playbyplay14$Season_Type=="Regular",]
+
+# Only Consider Shots
+
 playbyplay14 <- playbyplay14[playbyplay14$Shot_Outcome == 1 | playbyplay14$Shot_Outcome == 0, ]
+
+# Create shot counter
 playbyplay14$shot_count <- with(playbyplay14, ave(as.character(Game_id), Game_id, FUN= seq_along))
 
-
-
+# Repeat for 2015 ----------
 playbyplay15 <- playbyplay[playbyplay$Season==2015,]
 playbyplay15 <- playbyplay15[playbyplay15$Season_Type=="Regular",]
+
 playbyplay15 <- playbyplay15[playbyplay15$Shot_Outcome == 1 | playbyplay15$Shot_Outcome == 0, ]
+
 playbyplay15$shot_count <- with(playbyplay15, ave(as.character(Game_id), Game_id, FUN= seq_along))
 
+# Repeat for 2016 ----------
 
 playbyplay16 <- playbyplay[playbyplay$Season==2016,]
 playbyplay16 <- playbyplay16[playbyplay16$Season_Type=="Regular",]
-playbyplay16 <- playbyplay16[playbyplay16$Shot_Outcome == 1 | playbyplay16$Shot_Outcome == 0, ]
-playbyplay16$shot_count <- with(playbyplay16, ave(as.character(Game_id), Game_id, FUN= seq_along))
 
+playbyplay16 <- playbyplay16[playbyplay16$Shot_Outcome == 1 | playbyplay16$Shot_Outcome == 0, ]
+
+playbyplay16$shot_count <- with(playbyplay16, ave(as.character(Game_id), Game_id, FUN= seq_along))
 
 # Merge Files
 
-# Merge by game id and filter by period
-library(dplyr)
+# Merge by game id and shot count
+
 Assist14 <- playbyplay14 %>%
   inner_join(shot2014, by= c("Game_id"="GAME_ID", "shot_count"= "shot_count"))
 
@@ -74,5 +98,6 @@ Assist15 <- playbyplay15 %>%
 
 Assist16 <- playbyplay16 %>%
   inner_join(shot2016, by= c("Game_id"="GAME_ID", "shot_count"= "shot_count"))
+
 
 
